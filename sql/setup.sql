e03c1a423ae39c336c88e6db788c4bc57ee6da3b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-01 10:20:06 -0600

    Add cascade to users table drop

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,7 +11,7 @@ CREATE TABLE users (
 
 CREATE TABLE items (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id BIGINT ,
+  user_id BIGINT,
   description VARCHAR NOT NULL,
   bought BOOLEAN NOT NULL DEFAULT(false),
   qty INT,
